70ef18ace0e0d919a6ceae4596d35f40f7103779
Andrea Minato <aminato@tempestive.com>
2022-05-04 13:04:33 +0200

    With text inside

--- src/App.vue
+++ src/App.vue
@@ -54,6 +54,23 @@ function getSectorPath(
 
   return `M${x} ${y} ${cx1} ${cy1} A${cr} ${cr} 0 0 1 ${cx2} ${cy2}Z`;
 }
+function getReducedSectorPath(
+  x: number,
+  y: number,
+  outerDiameter: number,
+  a1: number,
+  a2: number
+) {
+  const degtorad = Math.PI / 180;
+  const halfOuterDiameter = outerDiameter / 2;
+  const cr = halfOuterDiameter - 10;
+  const cx1 = Math.cos(degtorad * a2) * cr + x;
+  const cy1 = -Math.sin(degtorad * a2) * cr + y;
+  const cx2 = Math.cos(degtorad * a1) * cr + x;
+  const cy2 = -Math.sin(degtorad * a1) * cr + y;
+
+  return `M${x} ${y} ${cx1} ${cy1} A${cr} ${cr} 0 0 1 ${cx2} ${cy2}Z`;
+}
 
 function spinWheel() {
   if (spinning.value) return;
@@ -133,7 +150,7 @@ const wheelSlices = computed(() => {
                 v-for="slice in wheelSlices"
                 :key="slice.title"
                 :d="
-                  getSectorPath(
+                  getReducedSectorPath(
                     outerDiameter / 2,
                     outerDiameter / 2,
                     outerDiameter,
