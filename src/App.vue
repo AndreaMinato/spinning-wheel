894efd7e71244ec7da18958b471e60df1f6155e5
Andrea Minato <15440218+AndreaMinato@users.noreply.github.com>
2022-05-12 10:23:05 +0200

    Nuove penitenze

--- src/App.vue
+++ src/App.vue
@@ -20,32 +20,41 @@ function setIsOpen(value: boolean) {
   }
 }
 
+// 1. Convinci i turisti a sposarsi (spiega ai turisti i motivi per cui dovrebbero sposarsi) 
+// 2. Indovinello 
+// 3. Balla al ritmo di una delle tue hit preferite! 
+// 4. Cantaci una delle te canzoni preferite! 
+// 5. Allenati per il matrimonio (esercizi di ginnastica artistica per ricordare la tua infanzia) 
+// 6. Fai l’infermiera (prendi parametri vitali alle persone e rassicurali del loro stato di salute) 
+// 7. Cos’è una colonscopia? (spiega alle persone in cosa consiste quest’interessante esame) 
+// 8. Esame della vista 
+// 9. 3, 2, 1…AZIONE! (recita una parte del copione per cui hai tanto lottato)
+
 const baseOptions: Array<Opzione> = [
   {
-    title: "flessioni",
-    description: "flessioni => Lorem ipsum dolo sit amet.",
+    title: "Sposatevi tutti",
+    description: "Convinci i turisti a sposarsi, spiega loro i motivi per cui dovrebbero sposarsi",
   },
-  { title: "biscotti", description: "biscotti => Lorem ipsum dolo sit amet." },
+  { title: "Indovinello", description: "" },
   {
-    title: "colonscopia",
-    description: "colonscopia => Lorem ipsum dolo sit amet.",
-  },
-  {
-    title: "altre cose divertenti",
-    description: "altre cose divertenti => Lorem ipsum dolo sit amet.",
+    title: "Balla",
+    description: "Balla al ritmo di una delle tue hit preferite!",
   },
   {
-    title: "ma molto divertenti",
-    description: "ma molto divertenti => Lorem ipsum dolo sit amet.",
+    title: "Canta",
+    description: "Cantaci una delle te canzoni preferite!",
   },
   {
-    title: "fai le puzze",
-    description: "fai le puzze => Lorem ipsum dolo sit amet.",
+    title: "Che fatica!",
+    description: "Allenati per il matrimonio, ricorda la tua infanzia con qualche esercizio di ginnastica artistica",
   },
-  { title: "1", description: "1 => Lorem ipsum dolo sit amet." },
-  { title: "2", description: "2 => Lorem ipsum dolo sit amet." },
-  { title: "3", description: "3 => Lorem ipsum dolo sit amet." },
-  { title: "4", description: "4 => Lorem ipsum dolo sit amet." },
+  {
+    title: "Infermieraa!",
+    description: "Fai l'infermiera, prendi parametri vitali alle persone e rassicurali del loro stato di salute",
+  },
+  { title: "Cos'è una colonscopia?", description: "Spiega alle persone in cosa consiste quest'interessante esame" },
+  { title: "Esame della vista", description: "" },
+  { title: "3, 2, 1…AZIONE!", description: "Recita una parte del copione per cui hai tanto lottato" },
 ];
 
 const options = useLocalStorage<Array<Opzione>>(
@@ -156,69 +165,42 @@ const wheelSlices = computed(() => {
 
     <main class="mx-auto max-w-xl w-full">
       <div class="relative w-full aspect-square -rotate-90">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
+        <svg xmlns="http://www.w3.org/2000/svg"
           class="rotate-90 absolute bottom-0 top-0 my-auto aspect-square w-[6%] z-50 text-cyan-600 right-[0.5%]"
-          viewBox="0 0 24 24"
-        >
+          viewBox="0 0 24 24">
           <g fill="currentColor">
             <path
-              d="M12 0a9.5 9.5 0 0 0-4 18.119 2.5 2.5 0 0 1 1.218 1.222l1.873 4.076a1 1 0 0 0 1.817 0l1.876-4.08A2.5 2.5 0 0 1 16 18.118 9.5 9.5 0 0 0 12 0zm4.8 8.2l-2.043 2.015a.249.249 0 0 0-.054.277l1.132 2.6a.654.654 0 0 1-.931.819l-2.778-1.565a.249.249 0 0 0-.245 0L9.1 13.912a.654.654 0 0 1-.931-.819l1.132-2.6a.249.249 0 0 0-.054-.277L7.2 8.2a.613.613 0 0 1 .429-1.07h2.339a.25.25 0 0 0 .227-.144l1.217-2.625a.662.662 0 0 1 1.177 0L13.8 6.982a.248.248 0 0 0 .226.145h2.336A.613.613 0 0 1 16.8 8.2z"
-            ></path>
+              d="M12 0a9.5 9.5 0 0 0-4 18.119 2.5 2.5 0 0 1 1.218 1.222l1.873 4.076a1 1 0 0 0 1.817 0l1.876-4.08A2.5 2.5 0 0 1 16 18.118 9.5 9.5 0 0 0 12 0zm4.8 8.2l-2.043 2.015a.249.249 0 0 0-.054.277l1.132 2.6a.654.654 0 0 1-.931.819l-2.778-1.565a.249.249 0 0 0-.245 0L9.1 13.912a.654.654 0 0 1-.931-.819l1.132-2.6a.249.249 0 0 0-.054-.277L7.2 8.2a.613.613 0 0 1 .429-1.07h2.339a.25.25 0 0 0 .227-.144l1.217-2.625a.662.662 0 0 1 1.177 0L13.8 6.982a.248.248 0 0 0 .226.145h2.336A.613.613 0 0 1 16.8 8.2z">
+            </path>
           </g>
         </svg>
 
-        <div
-          class="h-full w-full spin-me"
-          ref="spinner"
-          :style="{ '--finish-angle': `${spinningAngle}deg` }"
-        >
-          <svg
-            class="w-full h-full"
-            :viewBox="`0 0 ${outerDiameter} ${outerDiameter}`"
-          >
+        <div class="h-full w-full spin-me" ref="spinner" :style="{ '--finish-angle': `${spinningAngle}deg` }">
+          <svg class="w-full h-full" :viewBox="`0 0 ${outerDiameter} ${outerDiameter}`">
             <defs>
-              <path
-                v-for="slice in wheelSlices"
-                :key="slice.title"
-                :d="
-                  getSectorPath(
-                    outerDiameter / 2,
-                    outerDiameter / 2,
-                    outerDiameter,
-                    slice.start,
-                    slice.end,
-                    9
-                  )
-                "
-                :id="slice.title"
-              />
-            </defs>
-            <path
-              v-for="slice in wheelSlices"
-              :key="slice.title"
-              :d="
+              <path v-for="slice in wheelSlices" :key="slice.title" :d="
                 getSectorPath(
                   outerDiameter / 2,
                   outerDiameter / 2,
                   outerDiameter,
                   slice.start,
-                  slice.end
+                  slice.end,
+                  9
                 )
-              "
-              :fill="slice.color"
-            />
+              " :id="slice.title" />
+            </defs>
+            <path v-for="slice in wheelSlices" :key="slice.title" :d="
+              getSectorPath(
+                outerDiameter / 2,
+                outerDiameter / 2,
+                outerDiameter,
+                slice.start,
+                slice.end
+              )
+            " :fill="slice.color" />
 
-            <text
-              font-size="2px"
-              v-for="slice in wheelSlices"
-              :key="slice.title"
-            >
-              <textPath
-                :href="`#${slice.title}`"
-                startOffset="50%"
-                text-anchor="middle"
-              >
+            <text font-size="2px" v-for="slice in wheelSlices" :key="slice.title">
+              <textPath :href="`#${slice.title}`" startOffset="50%" text-anchor="middle">
                 {{ slice.title }}
               </textPath>
             </text>
@@ -229,17 +211,13 @@ const wheelSlices = computed(() => {
 
     <footer class="mx-auto max-w-xl w-full mt-6">
       <div class="flex flex-col gap-2">
-        <button
-          class="w-full bg-cyan-600 py-2 rounded-lg text-white uppercase font-semibold tracking-wide"
-          @click="spinWheel"
-        >
+        <button class="w-full bg-cyan-600 py-2 rounded-lg text-white uppercase font-semibold tracking-wide"
+          @click="spinWheel">
           Tenta la fortuna
         </button>
 
-        <button
-          class="w-full bg-cyan-50 border py-2 rounded-lg text-cyan-600 font-semibold tracking-wide"
-          @click="reset"
-        >
+        <button class="w-full bg-cyan-50 border py-2 rounded-lg text-cyan-600 font-semibold tracking-wide"
+          @click="reset">
           Reset
         </button>
       </div>
@@ -248,26 +226,19 @@ const wheelSlices = computed(() => {
     <dialog ref="dialog" class="relative z-50">
       <div class="fixed inset-0 flex items-center justify-center p-4">
         <div
-          class="relative inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl my-8 max-w-sm w-full p-6"
-        >
+          class="relative inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl my-8 max-w-sm w-full p-6">
           <h3 class="text-lg leading-6 font-medium text-gray-900">
             {{ currentlyOn.title }}
           </h3>
           <p class="text-sm text-gray-500">{{ currentlyOn.description }}</p>
 
           <div class="mt-5 sm:mt-6 flex flex-col gap-3">
-            <button
-              @click="rimuovi"
-              type="button"
-              class="w-full bg-cyan-50 border py-2 rounded-lg text-cyan-600 font-semibold tracking-wide"
-            >
+            <button @click="rimuovi" type="button"
+              class="w-full bg-cyan-50 border py-2 rounded-lg text-cyan-600 font-semibold tracking-wide">
               Rimuovi dalle opzioni
             </button>
-            <button
-              @click="setIsOpen(false)"
-              type="button"
-              class="w-full bg-cyan-50 border py-2 rounded-lg text-cyan-600 font-semibold tracking-wide"
-            >
+            <button @click="setIsOpen(false)" type="button"
+              class="w-full bg-cyan-50 border py-2 rounded-lg text-cyan-600 font-semibold tracking-wide">
               Ok
             </button>
           </div>
